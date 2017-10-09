4d559dcdbd2d88d4056a4a36e50a9f0ffb74a40b
Jake McGinty <me@jake.su>
2017-10-09 15:12:01 -0700

    x25519-dalek support

--- src/wrappers/crypto_wrapper.rs
+++ src/wrappers/crypto_wrapper.rs
@@ -1,15 +1,16 @@
 extern crate crypto;
 extern crate blake2_rfc;
 extern crate chacha20_poly1305_aead;
+extern crate x25519_dalek;
 
 use self::crypto::digest::Digest;
 use self::crypto::sha2::{Sha256, Sha512};
 use self::crypto::aes::KeySize;
 use self::crypto::aes_gcm::AesGcm;
 use self::crypto::aead::{AeadEncryptor, AeadDecryptor};
-use self::crypto::curve25519::{curve25519, curve25519_base};
 use self::blake2_rfc::blake2b::Blake2b;
 use self::blake2_rfc::blake2s::Blake2s;
+use self::x25519_dalek as x25519;
 
 use byteorder::{ByteOrder, BigEndian, LittleEndian};
 
@@ -67,8 +68,8 @@ impl Dh for Dh25519 {
 
     fn set(&mut self, privkey: &[u8]) {
         copy_memory(privkey, &mut self.privkey); /* RUSTSUCKS: Why can't I convert slice -> array? */
-        let pubkey = curve25519_base(&self.privkey);
-        copy_memory(&pubkey, &mut self.pubkey);
+        let pubkey = x25519::generate_public(&self.privkey);
+        copy_memory(pubkey.as_bytes(), &mut self.pubkey);
     }
 
     fn generate(&mut self, rng: &mut Random) {
@@ -76,8 +77,8 @@ impl Dh for Dh25519 {
         self.privkey[0]  &= 248;
         self.privkey[31] &= 127;
         self.privkey[31] |= 64;
-        let pubkey = curve25519_base(&self.privkey);
-        copy_memory(&pubkey, &mut self.pubkey);
+        let pubkey = x25519::generate_public(&self.privkey);
+        copy_memory(pubkey.as_bytes(), &mut self.pubkey);
     }
 
     fn pubkey(&self) -> &[u8] {
@@ -88,11 +89,13 @@ impl Dh for Dh25519 {
         &self.privkey
     }
 
+    // TODO avoid need to copy into sized array
     fn dh(&self, pubkey: &[u8], out: &mut [u8]) {
-        let result = curve25519(&self.privkey, pubkey);
+        let mut sized_pubkey = [0u8; 32];
+        copy_memory(&pubkey[..32], &mut sized_pubkey);
+        let result = x25519::diffie_hellman(&self.privkey, &sized_pubkey);
         copy_memory(&result, out);
     }
-
 }
 
 impl Cipher for CipherAESGCM {
